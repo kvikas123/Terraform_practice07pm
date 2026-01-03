f92798ce96826bd8371a841ddcaf6906a5c77b6d
vikas kumar <vikas.inetsolv@gmail.com>
2026-01-04 00:03:43 +0530

    day-3 local tfstate

---
+++ day-2/test.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-068c0051b15cdb816"
+inst_type =  "t2.micro"
---
+++ day-3_local_tfsatse/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "name" {
+    ami= var.ami_id
+    instance_type = var.inst_type
+    tags = {
+      name="test"
+    }
+
+  
+}
